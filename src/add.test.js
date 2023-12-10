b2a1a74168553c27a7c65bc53361aafdd03aa339
OGgrizzley <owengossen@hotmail.com>
2023-12-10 13:34:15 +0200

    Test commit for CI and coverage

---
+++ src/add.test.js
@@ -0,0 +1,30 @@
+// tests/add.test.js
+import add from './add.js';
+
+describe('add function', () => {
+  test('adds two positive numbers', () => {
+    expect(add(2, 3)).toBe(5);
+  });
+
+  test('adds a positive and a negative number', () => {
+    expect(add(-5, 8)).toBe(3);
+  });
+
+  test('adds two negative numbers', () => {
+    expect(add(-3, -7)).toBe(-10);
+  });
+
+  test('adds a positive number and zero', () => {
+    expect(add(4, 0)).toBe(4);
+  });
+
+  test('adds zero and a negative number', () => {
+    expect(add(0, -6)).toBe(-6);
+  });
+
+  test('adds zero and zero', () => {
+    expect(add(0, 0)).toBe(0);
+  });
+});
+
+// You can add more tests to cover edge cases or specific scenarios
