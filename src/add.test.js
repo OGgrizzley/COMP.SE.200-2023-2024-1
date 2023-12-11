7b78396fd36f97aa5a0969fa98cedc964bcc7209
OGgrizzley <owengossen@hotmail.com>
2023-12-11 20:22:38 +0200

    Latest tests

--- src/add.test.js
+++ src/add.test.js
@@ -42,4 +42,33 @@ describe('add.js - Addition Functionality', () => {
     const result = add(1000000, 500000);
     expect(result).toBe(1500000);
   });
+
+  // Additional Test case 8: Handling undefined values as zero
+  test('Handles undefined values as zero', () => {
+    const result1 = add(undefined, 5);
+    const result2 = add(3, undefined);
+    const result3 = add(undefined, undefined);
+
+    expect(result1).toBe(5);
+    expect(result2).toBe(3);
+    expect(result3).toBe(0);
+  });
+
+
+  // Additional Test case 9: Handling string representations of numbers
+  test('Handles string representations of numbers', () => {
+    const result1 = add('2', '3');
+    const result2 = add('6', 4);
+
+    expect(result1).toBe(5); // Now, it's a number
+    expect(result2).toBe(10); // Now, it's a number
+  });
+
+  // Additional Test case 10: Handling negative zero correctly
+  test('Handles negative zero correctly', () => {
+    const result = add(0, -0);
+    expect(Object.is(result, -0)).toBe(true); // Use Object.is directly
+  });
 });
+
+
